7374bb6c8f40c37d6c31877ed7b30569c5624734
HDpearlman <HDpearlman@users.noreply.github.com>
2020-04-23 00:00:26 -0400

    did not meet mvp in time

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,13 +1,24 @@
 // Write your Character component here
-import React from "react"
+import React from "react";
+import styled from 'styled-components';
 
+const CardDiv = styled.div`
+display:flex;
+border:2px solid black;
+background-color:white;
+width:25%;
+margin:auto;
+justify-content:center;
+`;
 
 const characterCard = props =>{
+    console.log(props)
+    
     return (
-        <div>
-           <h1>{props.name}</h1>
+        <CardDiv><h1>name</h1></CardDiv>
+           
           
-        </div>
+        
     )
 }
 
